ab90af77c494c7093e05b49ee339c9519390ad23
Priyam Verma <code.relatrino@gmail.com>
2025-09-17 13:19:27 +0530

    Added main.py and new.py

---
+++ main.py
@@ -0,0 +1,24 @@
+print('Hello, World!')
+
+## This is a single line comment
+
+'''
+This 
+is
+a
+multi-line
+comment
+'''
+
+total = 1+2+3+4+5+6+7+\
+    4+5+6
+print(total)
+
+x=5;y=16;z=x+y
+print(z)
+
+name = 'Priyam'
+age = 22
+print(type(name))
+print(type(age))
+
